c1a71cc4128c9881e069152e61e26613c61aec1e
Matt Jones <me@mattjones.tech>
2024-08-10 11:37:04 -0400

    update cards container, remove static html

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -7,21 +7,21 @@ const CardContainer = () => {
       <div className='container-xl lg:container m-auto'>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg'>
           <Card>
-            <h2 className='text-2xl font-bold'>For Developers</h2>
+            <h2 className='text-2xl font-bold'>For Buyers</h2>
             <p className='mt-2 mb-4'>
-              Browse our React jobs and start your career today
+              Explore the latest bikes and find your dream machine.
             </p>
             <a href="#" className='inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600'>
-              Browse Jobs
+              Browse Bikes
             </a>
           </Card>
           <Card>
-            <h2 className='text-2xl font-bold'>For Employers</h2>
+            <h2 className='text-2xl font-bold'>For Sellers</h2>
             <p className='mt-2 mb-4'>
-              List your job to find the perfect developer for the role
+              List your bike and start getting offers today.
             </p>
             <a href="#" className='inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600'>
-              Add Job
+              List Bike
             </a>
           </Card>
         </div>
